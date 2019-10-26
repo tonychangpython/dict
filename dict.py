4fb8dec4d806bd84696224b91d1e49f2013774f5
unknown <tony31318921@gmail.com>
2019-10-27 00:35:37 +0800

    add dictionary

---
+++ dict.py
@@ -0,0 +1,15 @@
+# dictionary 字典
+p0 = {
+	'name': '綠茶',
+	'price': '40'
+}
+
+p1 = {
+	'name': '珍珠奶茶',
+	'price': '50'
+
+}
+
+drinks = [p0, p1]
+print(drinks[0]['name'])
+print(drinks[1]['price'])
